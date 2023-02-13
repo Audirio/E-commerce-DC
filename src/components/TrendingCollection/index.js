089ef06f7df19ac044f38358a982acf1702f5518
Audirio <audiriobezerra@gmail.com>
2023-02-13 18:25:15 -0300

    colecao destaque

--- src/components/TrendingCollection/index.js
+++ src/components/TrendingCollection/index.js
@@ -1,21 +1,29 @@
 import { Grid } from "@mui/material";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 import "./styles.scss";
+
+
+function CadaCategoria(props) {
+
+}
 
 export default function TrendingCollection() {
     const [categorias, setCategorias] = useState([]);
     const [active, setActive] = useState(0);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('http://localhost:8000/trending-collections')
             .then(res => res.json())
             .then(res => setCategorias(res));
     }, []);
 
-    return(
+
+    return (
         <div className="component-trending-collection">
-           <p align="center">Coleções em Destaque</p>
+            <p align="center">Coleções em Destaque</p>
 
             <Grid container>
                 <Grid item xs={1}/>
@@ -24,7 +32,7 @@ export default function TrendingCollection() {
                     let classe = 'circle ';
 
                     if (active === c.id) {
-                        classe += 'active'
+                        classe += 'active';
                     }
 
                     return (
@@ -39,7 +47,5 @@ export default function TrendingCollection() {
                 })}
             </Grid>
         </div>
-
-
     );
 }
